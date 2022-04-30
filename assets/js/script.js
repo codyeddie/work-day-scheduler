6eb265f48db932a2c9e996f7ad7afaa3c26f1a23
Cody Eddie <buffbuck0427@gmail.com>
2022-04-30 13:24:33 -0600

    save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+// create global variables to use in functions 
 var currentHour = ["9 AM", "10 AM","11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
-var timeBlocks = $(".container")  
+var timeBlocks = $(".container") 
+var savedTask = {}
+var tasks = {} 
 
 // add curret date to page
 $("#currentDay").text(moment().format('MMMM Do YYYY'));
@@ -7,23 +10,36 @@ $("#currentDay").text(moment().format('MMMM Do YYYY'));
 // creating and appending time blocks to the page 
 var timeBlockGen = function() {
     for (var i = 0; i < currentHour.length; i++) {
-      var timeBlockEl = $("<div>")
-      .addClass("row time-block")
-      var hourEl =$("<h2>")
-      .text(currentHour[i])
-      .addClass("hour col 1")
-      var textEl = $('<textarea>')
-      $(textEl).attr("placeholder", "task")
-      .text("")
-      .addClass("col-8 description")
-      var saveBtn = $('<button>')
-      .text("Save Task")
-      .addClass("col-1 saveBtn")
-      timeBlocks.append(timeBlockEl)
-      timeBlockEl.append(hourEl)
-      timeBlockEl.append(textEl)
-      timeBlockEl.append(saveBtn)
+        var timeBlockEl = $("<div>")
+        .addClass("row time-block")
+        var hourEl =$("<h2>")
+        .text(currentHour[i])
+        .addClass("hour col 1")
+        var textEl = $('<textarea>')
+        $(textEl).attr("placeholder", "task")
+        .text("")
+        .addClass("col-8 description")
+        var saveBtn = $('<button>')
+        .text("Save Task")
+        .addClass("col-1 saveBtn")
+        timeBlocks.append(timeBlockEl)
+        timeBlockEl.append(hourEl)
+        timeBlockEl.append(textEl)
+        timeBlockEl.append(saveBtn)
+        
+        //create an on click event for save button that will save tasks to local storage 
+        saveBtn.click(function(t){
+            console.log("click");
+            var taskName = t.target.previousElementSibling.value
+            var taskHour = $(this).siblings(".hour").text()
+            var savedTask =  {
+              time: taskHour,
+              name: taskName
+            }
+            tasks[savedTask.time] = savedTask.name
+            localStorage.setItem("Tasks", JSON.stringify(tasks));
+        });
     }
-  };
-
-  timeBlockGen()
+};
+timeBlockGen()
+  
